d0e9d6c4f131ac3959c34af87940235630c38c4c
Ashley Colman <Ashleyc97@outlook.com>
2019-04-18 17:13:22 +0100

    Editor v9 - New buttons and shrinked timeline size

---
+++ 3D Gameplay/Assets/Timeline.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timeline : MonoBehaviour {
+
+    public GameObject timeline;
+    Vector3 timelineCurrentPosition;
+    float x;
+
+    // Update is called once per frame
+    void Update () {
+		
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            x = timeline.transform.position.x;
+            float y = timeline.transform.position.y;
+            float z = timeline.transform.position.z;
+
+            x -= 100;
+            Vector3 newTimelinePosition = new Vector3(x, y, z);
+            timeline.transform.position = newTimelinePosition;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            x = timeline.transform.position.x;
+            float y = timeline.transform.position.y;
+            float z = timeline.transform.position.z;
+
+            x += 100;
+            Vector3 newTimelinePosition = new Vector3(x, y, z);
+            timeline.transform.position = newTimelinePosition;
+        }
+    }
+}
